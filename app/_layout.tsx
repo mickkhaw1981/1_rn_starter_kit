cfc45bb47319ff78e396db1da308abeb29cb350e
Mike Khaw <mikekhawnow@gmail.com>
2025-03-11 21:10:17 -0700

    fonts and splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,35 @@
-import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import "./global.css";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-//SplashScreen.preventAutoHideAsync();
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Rubik-Light": require("@/assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Regular": require("@/assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-Medium": require("@/assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-SemiBold": require("@/assets/fonts/Rubik-SemiBold.ttf"),
+    "Rubik-Bold": require("@/assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("@/assets/fonts/Rubik-ExtraBold.ttf"),
+  });
+
+  // Keep the splash screen visible while we fetch resources
+  SplashScreen.preventAutoHideAsync();
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik-Regular", "sans-serif"],
+        "rubik-bold": ["Rubik-Bold", "sans-serif"],
+        "rubik-extrabold": ["Rubik-ExtraBold", "sans-serif"],
+        "rubik-medium": ["Rubik-Medium", "sans-serif"],
+        "rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
+        "rubik-light": ["Rubik-Light", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
